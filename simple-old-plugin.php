469577a65bb59fb13b4e5010dcac70d36d3a4460
Kris Rutherford <rutherfordkl@myips.org>
2020-12-04 23:16:36 -0500

    Update simple-old-plugin.php

--- simple-old-plugin.php
+++ simple-old-plugin.php
@@ -1,5 +1,10 @@
 <?php
+<<<<<<< Updated upstream
 /* * Plugin Name: Simple RSS Feed to Post 
+=======
+/* 
+* Plugin Name: Simple RSS Feed to Post 
+>>>>>>> Stashed changes
 * Description: Plugin that creates posts from another WordPress RSS feed.
 * Version: 1.0 
 * License: GPL v3 or later 
@@ -8,19 +13,32 @@
 * Domain Path: /languages 
 */
 
+<<<<<<< Updated upstream
 
 // RSS Feed to Post Admin menu creation and display callback
 add_action( 'admin_menu', 'srftp_create_options_page' );
 
 function srftp_create_options_page(){
+=======
+// RSS Feed to Post Admin menu creation and display callback
+add_action( 'admin_menu', 'srftp_admin_menu' );
+
+function srftp_admin_menu() {
+ 
+>>>>>>> Stashed changes
     add_options_page(
         'Simple RSS Feed to Post',            
         'Simple RSS Feed to Post',            
         'manage_options',            
         'srftp_options_page',   
+<<<<<<< Updated upstream
         'srftp_options_page_callback'
+=======
+        'srftp_display'
+>>>>>>> Stashed changes
     );
 }
+<<<<<<< Updated upstream
 
 function srftp_options_page_callback() {
 ?>
@@ -28,6 +46,17 @@ function srftp_options_page_callback() {
         <h2>Simple RSS Feed to Post Options</h2>
         <form action="options.php" method="post">
             <?php 
+=======
+
+ 
+function srftp_display() {
+?>
+
+<div class="wrap">
+    <h2>RSS Feed to Post Options</h2>
+    <form action="options.php" method="post">
+        <?php 
+>>>>>>> Stashed changes
                     settings_fields( 'srftp_options' );
                     do_settings_sections( 'srftp_options_page' );
                     submit_button('Save Changes', 'primary'); 
@@ -37,9 +66,13 @@ function srftp_options_page_callback() {
     <?php
 } 
 
+<<<<<<< Updated upstream
 // Create initial plugin admin settings and display callbacks
 add_action('admin_init', 'srftp_initialize_settings');
 function srftp_create_settings() {
+=======
+function srftp_initialize_settings() {
+>>>>>>> Stashed changes
     register_setting(
         'srftp_options',
         'srftp_options'
@@ -56,6 +89,7 @@ function srftp_create_settings() {
     add_settings_field( 
         'srftp_feed_url', 
         'RSS Feed URL',                           
+<<<<<<< Updated upstream
         'srftp_url_callback',  
         'srftp_options_page',    
         'srftp_settings_section'         
@@ -68,8 +102,25 @@ function srftp_create_settings() {
         'srftp_settings_section'
     );
 
+=======
+        'srftp_feed_url_callback',  
+        'srftp_options_page',    
+        'srftp_settings_section'         
+    );   
+    
+	add_settings_field( 
+        'srftp_default_user', 
+        'Default Post Author',                           
+        'srftp_default_user_callback',  
+        'srftp_options_page',    
+        'srftp_settings_section'         
+    );  
+    
+>>>>>>> Stashed changes
 } 
+// Create initial plugin admin settings and display callbacks
 
+<<<<<<< Updated upstream
 function srftp_settings_section_callback(){
     echo '<p>Please enter the url below to setup the feed to post.</p>';
 }
@@ -78,6 +129,16 @@ function srftp_url_callback() {
      
     $options = get_option('srftp_options');
     
+=======
+add_action('admin_init', 'srftp_initialize_settings');
+
+function srftp_settings_section_callback(){
+    echo '<p>Please enter the url below to setup the feed to post.</p>';
+}
+function srftp_feed_url_callback() {
+     
+    $options = get_option('srftp_options');    
+>>>>>>> Stashed changes
      
     echo '<input id="srftp_feed_url" name="srftp_options[srftp_feed_url]" type="text" value="' . 
         esc_attr(isset($options['srftp_feed_url']) ? $options['srftp_feed_url'] : '') . 
@@ -85,6 +146,7 @@ function srftp_url_callback() {
 }
 
 function srftp_default_user_callback(){
+<<<<<<< Updated upstream
     $options = get_option('srftp_options');
     
     wp_dropdown_users([
@@ -93,6 +155,15 @@ function srftp_default_user_callback(){
         'selected' => isset($options['srftp_default_user']) ? $options['srftp_default_user'] : 0,
         ]
     );
+=======
+    wp_dropdown_users([
+        'name' => 'srftp_options[srftp_default_user]',
+        'id' => 'srftp_default_user',
+        'selected' => srftp_default_author(),
+        ]
+    );
+	echo '<p>The current value is: '.srftp_default_author().'</p>';
+>>>>>>> Stashed changes
 }
 
 // Parse RSS Feed functionality
@@ -128,15 +199,37 @@ function srftp_parse_rss_feed($url){
 
 // Create new post from RSS entries functionality
 function srftp_create_post($post_arguments){
+<<<<<<< Updated upstream
     
     //send error on failure or post_id number on completion
     return wp_insert_post(srftp_post_template($post_arguments),true);
     
+=======
+    
+    //send error on failure or post_id number on completion
+    return wp_insert_post(srftp_post_template($post_arguments),true);
+    
+>>>>>>> Stashed changes
     }
 
 //Checks if a default user is set. If not, returns 0.
 function srftp_default_author(){
+<<<<<<< Updated upstream
     return isset($options['srftp_default_user']) ? $options['srftp_default_user'] : 0;
+=======
+	$options = get_option('srftp_options');
+    return isset($options['srftp_default_user']) ? $options['srftp_default_user'] : 0;
+}
+
+function srftp_post_template($post_arguments){
+          return  ['post_author' => srftp_default_author(),
+              'post_date' => $post_arguments['pubDate'],
+              'post_content' => $post_arguments['content'],
+              'post_title' => $post_arguments['title'],
+              'post_excerpt' => $post_arguments['description'],
+              'post_type' => 'post',               
+            ];
+>>>>>>> Stashed changes
 }
 
 function srftp_post_template($post_arguments){
