453b712b8971ec0c25c049085dcbd8c18b99992a
Kris Rutherford <rutherfordkl@myips.org>
2020-12-15 19:59:46 -0500

    Update simple-rss-feed-to-post.php

--- simple-rss-feed-to-post.php
+++ simple-rss-feed-to-post.php
@@ -29,8 +29,22 @@ function srftp_options_page_callback() {
 
 <div class="wrap">
     <h2>Simple RSS Feed to Post Options</h2>
+    <?php
+    $error_messages = get_option('srftp_rss_feed_errors');
+    if ($error_messages !== ''){
+        ?>
+    <div class="error notice">
+        <p>
+            <?php
+        sanitize_textarea_field($error_messages);
+    
+    ?>
+        </p>
+    </div>
+
+    <?php } ?>
     <form action="options.php" method="post">
-        <?php 
+        <?php       
                     settings_fields('srftp_options_page_settings');
                     do_settings_sections('srftp_options_page');
                     submit_button('Save Changes', 'primary'); 
@@ -43,21 +57,20 @@ function srftp_options_page_callback() {
 // Create initial plugin admin settings and display callbacks
 add_action('admin_init', 'srftp_initialize_options_page_settings');
 function srftp_initialize_options_page_settings() {
+    add_option('srftp_rss_feed_errors', '');
     
     register_setting(
         'srftp_options_page_settings',
         'srftp_options_page_settings',
-        srftp_options_page_section_args()
+        ['sanitize_callback' => 'srftp_sanitize_options_page_settings']
     );
     
     add_settings_section(
         'srftp_settings_section',        
-        'Simple RSS Feed to Post Settings',                  
+        'Settings',                 
         'srftp_settings_section_callback', 
         'srftp_options_page'    
     );
-     
-
     add_settings_field( 
         'srftp_feed_url', 
         'RSS Feed URL',                           
@@ -78,7 +91,7 @@ function srftp_initialize_options_page_settings() {
 }     
 
 function srftp_settings_section_callback(){
-    echo '<p>Please enter the url below to setup the feed to post.</p>';
+    echo '<p>Please enter a valid RSS url and select a default user for imported posts.</p>';
 }
 function srftp_url_callback() {
      
@@ -128,11 +141,22 @@ function srftp_sanitize_options_page_settings($submitted_fields){
     
 }
 
-function srftp_options_page_section_args(){
-    return ['sanitize_callback' => 'srftp_sanitize_options_page_settings'];
-}
 // Parse RSS Feed functionality
 function srftp_load_rss_from_url($url){
+    libxml_use_internal_errors(true);
+    $xml = simplexml_load_file($url);
+    
+    if ($xml===false){
+        $error_str='';
+        foreach(libxml_get_errors() as $error) {
+        $error_str .= (string)$error->message.'<br>';        
+            }
+        update_option('srftp_rss_feed_errors',wp_kses($error_str, ['br' => []]));
+        libxml_use_internal_errors(false);
+        return $xml;
+    }
+    update_option('srftp_rss_feed_errors','');
+    
     return simplexml_load_file($url);
 }
 
@@ -154,8 +178,7 @@ function srftp_parse_rss_feed_item($item){
     ];
 }
 
-function srftp_parse_rss_feed($url){
-    $xml = srftp_load_rss_from_url($url);
+function srftp_parse_rss_feed($xml){
     $feed_items = [];
     foreach ($xml -> channel -> item as $item){
         $feed_items[] = srftp_parse_rss_feed_item($item);
@@ -163,12 +186,6 @@ function srftp_parse_rss_feed($url){
     return $feed_items;
 }
 
-// Create new post from RSS entries functionality
-function srftp_create_post($post_arguments){    
-    //send error on failure or post_id number on completion
-    return wp_insert_post(srftp_post_template($post_arguments),true);
-
-    }
 
 //Checks if a default user is set. If not, returns 0.
 function srftp_default_author(){
@@ -183,8 +200,75 @@ function srftp_post_template($post_arguments){
               'post_content' => $post_arguments['content'],
               'post_title' => $post_arguments['title'],
               'post_excerpt' => $post_arguments['description'],
-              'post_type' => 'post',               
+              'post_type' => 'post', 
+              'meta_input' => ['srftp_source_url' => $post_arguments['guid']]
             ];
 }
 
-function srftp_check_if_post_exists
+// Create new post from RSS entries functionality
+function srftp_create_post($post_arguments){    
+    //send error on failure or post_id number on completion
+    return wp_insert_post(srftp_post_template($post_arguments),true);
+
+    }
+
+function srftp_get_added_posts(){
+$post_filters=['meta_value' => 'srftp_source_url'];
+
+    return get_posts($post_filters);
+    
+}
+
+// Add custom metadata field for posts and sanitization function
+function srftp_authorized_to_edit_meta_url($user){
+    return current_user_can('manage_options');
+}
+
+register_activation_hook(__FILE__,'srftp_register_post_source_url_meta');
+
+function srftp_register_post_source_url_meta(){
+    $meta_args = ['type' => 'string',
+                 'description' => 'Source url for imported post.',
+                 'single' => true,
+                 'sanitize_callback' => function($url){return esc_url_raw($url);},
+                  //'auth_callback' => 'srftp_authorized_to_edit_meta_url'
+                 ];
+    register_post_meta('post', 'srftp_source_url', $meta_args);
+}
+
+
+// Cron setup
+register_activation_hook(__FILE__,'srftp_cron_schedule');
+
+function srftp_cron_schedule(){
+    if(!wp_next_scheduled('srftp_cron_hook')){
+        wp_schedule_event(time(),'hourly','srftp_cron_hook');
+    }
+}
+
+add_action('srftp_cron_hook', 'srftp_rss_processing_orchestration');
+
+function srftp_rss_filter_items($rss_array, $wp_array){
+    return array_udiff($rss_array, $wp_array,
+                function($arg1,$arg2){
+                    return strcmp($arg1['guid'],get_post_meta($arg2 -> ID,'srftp_create_post',true));
+                }
+               );
+}
+function srftp_rss_processing_orchestration(){}
+function srftp_rss_processing_orchestration_work(){
+    $options = get_option('srftp_options_page_settings');
+    
+    if(array_key_exists('srftp_feed_url',$options) && get_option('srftp_rss_feed_errors') === ''){
+        $xml=srftp_load_rss_from_url(esc_url_raw($options['srftp_feed_url']));
+        if($xml !== false){
+            $current_posts = srftp_get_added_posts();
+            $xml_items = srftp_parse_rss_feed($xml);
+            $filtered_posts = srftp_rss_filter_items($xml_items, $current_posts);
+            $post_ids = array_map($filtered_posts,'srftp_create_post');
+        }
+        
+    }
+}
+              
+add_action('update_option_srftp_options_page_settings', function($option,$old,$new){srftp_rss_processing_orchestration();},10,3);
